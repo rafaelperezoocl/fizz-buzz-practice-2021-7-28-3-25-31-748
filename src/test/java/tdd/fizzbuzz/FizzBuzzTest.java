315063a4cf2abab2837f78ea99932392c0122579
RAFAEL PEREZ (GEDI-ISD-OOCLL/MNL) <PEREZRA@oocl.com>
2022-09-01 14:13:37 +0800

    test: testfail

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_number_1()
+    {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String expected = "1";
+
+
+        String actual = fizzbuzz.countOff(1);
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
