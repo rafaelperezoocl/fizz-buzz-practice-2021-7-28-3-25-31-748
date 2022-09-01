a8afaae0da992254b6099c1f4a1c97d57ee4029e
RAFAEL PEREZ (GEDI-ISD-OOCLL/MNL) <PEREZRA@oocl.com>
2022-09-01 14:44:15 +0800

    Feat: add condition in countOff method for Fizz test cases

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int order) {
+        if(order % 3 == 0)
+        {
+            return "Fizz";
 
+        }
 
         return String.valueOf(order);
     }
